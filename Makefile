f91ea8c53a31ae17ac39c70dbb3bba18195c8615
Dalfany <nycruz@gmail.com>
2025-01-13 11:44:46 -0500

    fix: comment out unsupported gpt models

--- Makefile
+++ Makefile
@@ -7,13 +7,14 @@ build:
 dev-gpt:
 	go run *.go --model=gpt
 
-dev-gpt-o1:
-	go run *.go --model=gpt-o1
-
-dev-gpt-o1-mini:
-	go run *.go --model=gpt-o1-mini
-
 dev-claude:
 	go run *.go --model=claude
+
+# dev-gpt-o1:
+# 	go run *.go --model=gpt-o1
+#
+# dev-gpt-o1-mini:
+# 	go run *.go --model=gpt-o1-mini
+
 
 .PHONY: all test clean run dev-gpt dev-claude dev-gpto1 dev-gpto1-mini
--- internal/models/models.go
+++ internal/models/models.go
@@ -8,15 +8,16 @@ const (
 	ModelGPTName      Model  = "gpt-4o"
 	ModelGPTMaxTokens Token  = 16384
 
-	ModelGPTo1          string = "gpt-o1"
-	ModelGPTo1Name      Model  = "o1"
-	ModelGPTo1MaxTokens Token  = 100000
-
-	ModelGPTo1Mini          string = "gpt-o1-mini"
-	ModelGPTo1MiniName      Model  = "o1-mini"
-	ModelGPTo1MiniMaxTokens Token  = 65536
-
 	ModelClaude          string = "claude"
 	ModelClaudeName      Model  = "claude-3-5-sonnet-latest"
 	ModelClaudeMaxTokens Token  = 8192
+
+	// ModelGPTo1          string = "gpt-o1"
+	// ModelGPTo1Name      Model  = "o1"
+	// ModelGPTo1MaxTokens Token  = 100000
+	//
+	// ModelGPTo1Mini          string = "gpt-o1-mini"
+	// ModelGPTo1MiniName      Model  = "o1-mini"
+	// ModelGPTo1MiniMaxTokens Token  = 65536
+
 )
