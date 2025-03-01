d34cf2fb2aef9da1c63b11e661791470e84a0817
Dalfany <nycruz@gmail.com>
2025-03-01 09:10:21 -0500

    feat: update claude model version

--- internal/models/models.go
+++ internal/models/models.go
@@ -9,15 +9,6 @@ const (
 	ModelGPTMaxTokens Token  = 16384
 
 	ModelClaude          string = "claude"
-	ModelClaudeName      Model  = "claude-3-5-sonnet-latest"
+	ModelClaudeName      Model  = "claude-3-7-sonnet-latest"
 	ModelClaudeMaxTokens Token  = 8192
-
-	// ModelGPTo1          string = "gpt-o1"
-	// ModelGPTo1Name      Model  = "o1"
-	// ModelGPTo1MaxTokens Token  = 100000
-	//
-	// ModelGPTo1Mini          string = "gpt-o1-mini"
-	// ModelGPTo1MiniName      Model  = "o1-mini"
-	// ModelGPTo1MiniMaxTokens Token  = 65536
-
 )
